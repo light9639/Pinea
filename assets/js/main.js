bb593dd288db40c5c6ef1fd4bb321c66d58c7ced
Lee dong ho <dong963939@gmail.com>
2023-12-10 21:53:30 +0900

    :art: 반응형 작성.
    반응형 작성.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,19 +29,30 @@ $('.header .item .title').click(function (e) {
 })
 
 Slide5 = new Swiper('.inner5 .swiper', {
-    slidesPerView: 2.75,
+    slidesPerView: 1.25,
     navigation: {
         nextEl: ".inner5 .btn-next",
         prevEl: ".inner5 .btn-prev"
     },
+    breakpoints: {
+        768: {
+            slidesPerView: 2.75,
+        },
+    },
 })
 
 Slide8 = new Swiper('.inner8 .swiper', {
-    slidesPerView: 4,
+    slidesPerView: 'auto',
     spaceBetween: 20,
     navigation: {
         nextEl: ".inner8 .btn-next",
         prevEl: ".inner8 .btn-prev"
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
     },
 })
 
@@ -83,90 +94,146 @@ logoTl.to('.header .logo', { transform: "translate(-50%, 0vh)" }, 'a')
 logoTl.to('.header .logo img', { width: 120 }, 'a')
 logoTl.to('.header', { background: "#2B2B2B" }, 'a')
 
+var ww = $(window).width();
+
 /**
  * container 2번째 문단 애니메이션 효과가 나오도록
  */
-parall_y_List = document.querySelectorAll('[data-parallax-y]')
-parall_x_List = document.querySelectorAll('[data-parallax-x]')
+function inner2Move() {
+    console.log(ww >= 768);
+    if (ww >= 768) {
+        parall_y_List = document.querySelectorAll('[data-parallax-y]')
+        parall_x_List = document.querySelectorAll('[data-parallax-x]')
 
-parall_x_List.forEach(element => {
-    gsap.to(element, {
-        scrollTrigger: {
-            trigger: element,//객체기준범위
-            start: "0% 100%",
-            end: "100% 0%",
-            scrub: 0,
-            marker: true
-        },
-        x: element.dataset.parallaxX,
-        ease: 'none'
-    });
-})
+        parall_x_List.forEach(element => {
+            gsap.to(element, {
+                scrollTrigger: {
+                    trigger: element,//객체기준범위
+                    start: "0% 100%",
+                    end: "100% 0%",
+                    scrub: 0,
+                    marker: true
+                },
+                x: element.dataset.parallaxX,
+                ease: 'none'
+            });
+        })
 
-parall_y_List.forEach(element => {
-    gsap.to(element, {
-        scrollTrigger: {
-            trigger: element,//객체기준범위
-            start: "0% 100%",
-            end: "100% 0%",
-            scrub: 0,
-            marker: true
-        },
-        y: element.dataset.parallaxY,
-        ease: 'none'
-    });
-})
+        parall_y_List.forEach(element => {
+            gsap.to(element, {
+                scrollTrigger: {
+                    trigger: element,//객체기준범위
+                    start: "0% 100%",
+                    end: "100% 0%",
+                    scrub: 0,
+                    marker: true
+                },
+                y: element.dataset.parallaxY,
+                ease: 'none'
+            });
+        })
+    } else {
+        mobile_y_List = document.querySelectorAll('[data-mobile-y]')
+        mobile_x_List = document.querySelectorAll('[data-mobile-x]')
 
-/**
- * 
- */
-mapTl = gsap.timeline({
-    scrollTrigger: {
-        trigger: '.inner6',//객체기준범위
-        start: "0% 100%",
-        end: "100% 0%",
-        scrub: true,
-        onUpdate: self => {
-            // console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
-            progress = self.progress.toFixed(3) * 100;
-            direct = self.direction;
-            if (progress >= 75) {
-                $('.inner6 .img-area .content:nth-child(1)').addClass('on').siblings().removeClass('on');
-                if (direct > 0) {
-                    $('.inner6 .txt-area .link-more').addClass('on');
-                } else {
-                    return
-                }
-            } else if (progress >= 50) {
-                $('.inner6 .img-area .content:nth-child(2)').addClass('on').siblings().removeClass('on');
-                if (direct > 0) {
-                    $('.inner6 .txt-area .middle-text').addClass('on');
-                    $('.inner6 .btn-wrap button:nth-child(1)').addClass('on');
-                } else {
-                    $('.inner6 .txt-area .link-more').removeClass('on');
+        mobile_x_List.forEach(element => {
+            gsap.to(element, {
+                scrollTrigger: {
+                    trigger: element,//객체기준범위
+                    start: "0% 100%",
+                    end: "100% 0%",
+                    scrub: 0,
+                    marker: true
+                },
+                x: element.dataset.mobileX,
+                ease: 'none'
+            });
+        })
 
-                }
-            } else if (progress >= 25) {
-                $('.inner6 .img-area .content:nth-child(3)').addClass('on').siblings().removeClass('on');
-                if (direct > 0) {
-                    $('.inner6 .txt-area .Top-text').addClass('on');
-                    $('.inner6 .btn-wrap button:nth-child(2)').addClass('on');
-                } else {
-                    $('.inner6 .txt-area .middle-text').removeClass('on');
-                    $('.inner6 .btn-wrap button:nth-child(1)').removeClass('on');
+        mobile_y_List.forEach(element => {
+            gsap.to(element, {
+                scrollTrigger: {
+                    trigger: element,//객체기준범위
+                    start: "0% 100%",
+                    end: "100% 0%",
+                    scrub: 0,
+                    marker: true
+                },
+                y: element.dataset.mobileY,
+                ease: 'none'
+            });
+        })
+    }
+}
 
-                }
-            } else {
-                $('.inner6 .img-area .content:nth-child(4)').addClass('on').siblings().removeClass('on');
-                if (direct < 0) {
-                    $('.inner6 .txt-area .Top-text').removeClass('on');
-                    $('.inner6 .btn-wrap button:nth-child(2)').removeClass('on');
-                }
-            }
-        },
-        ease: 'none'
-    },
-})
+function inner6Move() {
+    if (ww >= 768) {
+        /**
+     * EL VALOR DEL 부분 고정시키고
+     */
+        mapTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.inner6',//객체기준범위
+                start: "0% 100%",
+                end: "100% 0%",
+                scrub: true,
+                onUpdate: self => {
+                    // console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+                    progress = self.progress.toFixed(3) * 100;
+                    direct = self.direction;
+                    if (progress >= 75) {
+                        $('.inner6 .img-area .content:nth-child(1)').addClass('on').siblings().removeClass('on');
+                        if (direct > 0) {
+                            $('.inner6 .txt-area .link-more').addClass('on');
+                        } else {
+                            return
+                        }
+                    } else if (progress >= 50) {
+                        $('.inner6 .img-area .content:nth-child(2)').addClass('on').siblings().removeClass('on');
+                        if (direct > 0) {
+                            $('.inner6 .txt-area .middle-text').addClass('on');
+                            $('.inner6 .btn-wrap button:nth-child(1)').addClass('on');
+                        } else {
+                            $('.inner6 .txt-area .link-more').removeClass('on');
+
+                        }
+                    } else if (progress >= 25) {
+                        $('.inner6 .img-area .content:nth-child(3)').addClass('on').siblings().removeClass('on');
+                        if (direct > 0) {
+                            $('.inner6 .txt-area .Top-text').addClass('on');
+                            $('.inner6 .btn-wrap button:nth-child(2)').addClass('on');
+                        } else {
+                            $('.inner6 .txt-area .middle-text').removeClass('on');
+                            $('.inner6 .btn-wrap button:nth-child(1)').removeClass('on');
+
+                        }
+                    } else {
+                        $('.inner6 .img-area .content:nth-child(4)').addClass('on').siblings().removeClass('on');
+                        if (direct < 0) {
+                            $('.inner6 .txt-area .Top-text').removeClass('on');
+                            $('.inner6 .btn-wrap button:nth-child(2)').removeClass('on');
+                        }
+                    }
+                },
+                ease: 'none'
+            },
+        })
+    } else {
+        return
+    }
+}
+
+$(window).on('load', function () {
+    inner2Move();
+    inner6Move();
+});
+
+$(window).on('resize', function () {
+    ww = $(window).width();
+    console.log(ww);
+    inner2Move();
+    inner6Move();
+});
 
 /**
  * EL VALOR DEL TERROIR 부분에 다다르면 html에 dark를 붙였다 떼어짐
@@ -176,8 +243,4 @@ ScrollTrigger.create({
     start: "0% 50%",
     end: "100% 50%",
     toggleClass: { targets: 'html', className: 'dark' }
-})
-
-window.addEventListener("resize", () => {
-    gsap.matchMediaRefresh()
 })
